28172305b2da9c9770f4c2ce12174eda1bd14bbd
jasti <vamseej@gmail.com>
2014-05-15 22:51:13 -0400

    modularized parse and groupby rest days

--- CrossparserUtils.py
+++ CrossparserUtils.py
@@ -7,13 +7,17 @@ Created on Thu May 15 11:00:58 2014
 
 from datetime import datetime
 import calendar
+import numpy as np
+from bs4 import BeautifulSoup 
+import requests
+import csv
 
 # Generates URLS that need to be queried for workouts
 def generateURL():
     crossfitBaseUrl = 'http://www.crossfit.com/mt-archive2/YYYY_MM.html'
-    yearRange = range(2002, 2003)
-    # monthRange = ['01','02','03','04','05','06','07','08','09','10','11','12']
-    monthRange = ['01']
+    yearRange = range(2002, 2014)
+    monthRange = ['01','02','03','04','05','06','07','08','09','10','11','12']
+    #monthRange = ['01']
     crossfitUrls = [];
     for year in yearRange:
         for month in monthRange:
@@ -36,6 +40,56 @@ def splitDate(date):
         return [mydate.year, calendar.month_name[mydate.month], mydate.day]
     except ValueError:
         print "Bad Date encountered : " + strDate
+        return [np.NaN,np.NaN,np.NaN]
+    
+def parseSite():
+    urls = generateURL();
+#print urls
+
+    with open("workouts.tsv", "w") as f:
+        fieldnames = ("Date", "Workout")
+        output = csv.writer(f, delimiter="\t")
+        output.writerow(fieldnames)
+        
+        
+        for url in urls:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text)
+            #print soup
+            blogBodies = soup.findAll("div", {"class" : "blogbody"})
+            for line  in blogBodies:
+                #print('\n~break~\n');
+              
+                workoutName = line.find("h3", {"class" : "title"})
+                #print workoutName
+                if workoutName is not None:
+                    if(len(workoutName.text.split()) < 2):
+                        # Some entries only have the day and not the actual date
+                        #workoutDate = np.nan;
+                        break;
+                    
+                    else:
+                        #Add 20 to the year part, so Pandas can easily extract the date format
+                        workoutDate = "20"+workoutName.text.split()[1];
+                  
+                    
+                    para =line.findAll('p')
+                    workout = "";
+                    for p in para:
+                        # Sometime their workouts have links and they need to be filtered for
+                        anchorExists = p.find("a")
+                        if (p.text=='Enlarge image') |('Post' in p.text)|('comments' in p.text)|(p.text == "") :
+                            break;
+                        else:
+                         
+                            workout = workout+p.text.encode('utf-8')
+                            
+                     
+                    output.writerow([workoutDate,workout.replace('\n',' ')])    
+                    print "~~" 
+                    print workoutDate
+                    print workout.rstrip('\n')
+    
    
         
 
--- Parser.py
+++ Parser.py
@@ -3,7 +3,7 @@ Created on Mar 27, 2013
 
 @author: zudec
 '''
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 import requests
 import csv
 import pandas as pd
@@ -11,54 +11,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import CrossparserUtils as cpu
 
-urls = cpu.generateURL();
-#print urls
-f = open('wods.txt','w')
+#cpu.parseSite();
 
-with open("workouts.tsv", "w") as f:
-    fieldnames = ("Date", "Workout")
-    output = csv.writer(f, delimiter="\t")
-    output.writerow(fieldnames)
-    
-    
-    for url in urls:
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text)
-        #print soup
-        blogBodies = soup.findAll("div", {"class" : "blogbody"})
-        for line  in blogBodies:
-            #print('\n~break~\n');
-          
-            workoutName = line.find("h3", {"class" : "title"})
-            #print workoutName
-            if(len(workoutName.text.split()) < 2):
-                # Some entries only have the day and not the actual date
-                #workoutDate = np.nan;
-                break;
-            
-            else:
-                #Add 20 to the year part, so Pandas can easily extract the date format
-                workoutDate = "20"+workoutName.text.split()[1];
-          
-            
-            para =line.findAll('p')
-            workout = "";
-            for p in para:
-                # Sometime their workouts have links and they need to be filtered for
-                anchorExists = p.find("a")
-                if (p.text=='Enlarge image') |('Post' in p.text)|('comments' in p.text)|(p.text == "") :
-                    break;
-                else:
-                 
-                    workout = workout+p.text.encode('utf-8')
-                    
-             
-            output.writerow([workoutDate,workout.replace('\n',' ')])    
-            #print "~~"
-            #print workoutDate
-            #print workout.rstrip('\n')
-
-
+print "Finished writing to the file"
 
 cols =['Date','Workout']
 df = pd.read_csv('workouts.tsv', sep='\t', converters={'Date': str})  
@@ -68,12 +23,18 @@ df['Year'], df['Month'], df['Day'] = zip(*df["Date"].map(cpu.splitDate))
 
 print df
 #dfRest=df[df.Workout== "Rest Day"]
-print  df[df['Workout'].str.contains("Rest Day")]
+dfRest = df[df['Workout'].str.contains("Rest Day") | df['Workout'].str.contains("Rest day")]
+
+
+gb_year_month =dfRest.groupby(['Year','Month'])
+print gb_year_month.size()
 
 '''
-gb_year =dfRest.groupby('Year')
-print gb_year.size()
-
+for i, group in dfRest.groupby(['Year','Month']):
+    plt.figure()
+    group.plot(x='saleDate', y='MeanToDate', title=str(i))
+'''
+'''
 df.dropna()
 df[df['Workout'].str.contains("For time")]
 '''
